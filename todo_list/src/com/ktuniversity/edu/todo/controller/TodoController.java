4a53f2d4f0d264ab77d5741e19263660a5b0f174
codingjds <122941424+codingjds@users.noreply.github.com>
2023-02-01 14:26:40 +0900

    Update TodoController.java

--- todo_list/src/com/ktuniversity/edu/todo/controller/TodoController.java
+++ todo_list/src/com/ktuniversity/edu/todo/controller/TodoController.java
@@ -24,73 +24,81 @@ public class TodoController {
 		 * 7.종료
 		 */
 		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.println("=============");
+			System.out.println("1. 등록");
+			System.out.println("2. 수정");
+			System.out.println("3. 삭제");
+			System.out.println("4. 조회");
+			System.out.println("5. 종료");
+			System.out.println("=============");
+			System.out.println("메뉴를 입력하세요:");
 		
-		System.out.println("메뉴를 입력하세요.");
-		int menu = scan.nextInt();
-		scan.nextLine();
-		
-		if(menu == 1) {
-			System.out.println("TODO 명을 입력하세요.");
-			String todo = scan.nextLine();
-			if(service.create(todo)) {
-				System.out.println("아이템이 등록되었습니다.");
-			}
-		}
-		else if(menu == 2) {
-			System.out.println("조회할 TODO key를 입력하세요.");
-			int key = scan.nextInt();
-			scan.nextLine();
-			TodoVO todoVO = service.read(key);
-			//TODO 출력문 쓸자리
-		}
-		else if(menu == 3) {
-			List<TodoVO> todoList = service.readAll();
-			//TODO 출력문 자리
-		}
-		else if(menu == 4) {
-			System.out.println("수정할 TODO key를 입력하세요.");
-			int key = scan.nextInt();
+			int menu = scan.nextInt();
 			scan.nextLine();
 			
-			TodoVO todo = service.read(key);
-			if(todo == null) {
-				System.out.println("아이템이 존재하지 않습니다.");
+			if(menu == 1) {
+				System.out.println("TODO 명을 입력하세요.");
+				String todo = scan.nextLine();
+				if(service.create(todo)) {
+					System.out.println("아이템이 등록되었습니다.");
+				}
 			}
-			else {
+			else if(menu == 2) {
+				System.out.println("조회할 TODO key를 입력하세요.");
+				int key = scan.nextInt();
+				scan.nextLine();
+				TodoVO todoVO = service.read(key);
+				//TODO 출력문 쓸자리
+			}
+			else if(menu == 3) {
+				List<TodoVO> todoList = service.readAll();
+				//TODO 출력문 자리
+			}
+			else if(menu == 4) {
+				System.out.println("수정할 TODO key를 입력하세요.");
+				int key = scan.nextInt();
+				scan.nextLine();
 				
-				boolean isSuccess = false;
-				
-				if(todo.isSuccess()) {
-					//이미 완료된 건
-					System.out.println("이미 완료된 건입니다. 미완료로 변경할까요? (Y/N)");
-					String yn = scan.nextLine();
-					if(yn.equalsIgnoreCase("Y")) {
-						isSuccess = service.update(key, false);
-					}
+				TodoVO todo = service.read(key);
+				if(todo == null) {
+					System.out.println("아이템이 존재하지 않습니다.");
 				}
 				else {
-					//완료되지 않은 건
-					System.out.println("미완료된 건입니다. 완료로 변경할까요? (Y/N)");
-					String yn = scan.nextLine();
-					if(yn.equalsIgnoreCase("Y")) {
-						isSuccess = service.update(key, true);
+					
+					boolean isSuccess = false;
+					
+					if(todo.isSuccess()) {
+						//이미 완료된 건
+						System.out.println("이미 완료된 건입니다. 미완료로 변경할까요? (Y/N)");
+						String yn = scan.nextLine();
+						if(yn.equalsIgnoreCase("Y")) {
+							isSuccess = service.update(key, false);
+						}
+					}
+					else {
+						//완료되지 않은 건
+						System.out.println("미완료된 건입니다. 완료로 변경할까요? (Y/N)");
+						String yn = scan.nextLine();
+						if(yn.equalsIgnoreCase("Y")) {
+							isSuccess = service.update(key, true);
+						}
+					}
+					if(isSuccess) {
+						System.out.println("처리되었습니다.");
+					}
+						
 					}
 				}
-				if(isSuccess) {
-					System.out.println("처리되었습니다.");
+				else if(menu == 5) {
+					
 				}
-				
-			}
+				else if(menu == 6) {
+					
+				}
+				else if(menu == 7) {
+					
+				}
 		}
-		else if(menu == 5) {
-			
-		}
-		else if(menu == 6) {
-			
-		}
-		else if(menu == 7) {
-			
-		}
-		
 	}
 }
